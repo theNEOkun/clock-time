6c029090614734d87195b986ff86769628410a5f
theNEOkun <sven2000@student.miun.se>
2022-08-15 20:41:27 +0200

    Working on formatting and getting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod time;
 
+use time::Time;
+
 fn main() {
+    let time = Time::get_current_time();
+    println!("{}:{}:{}", Time::format_hours(&time), Time::format_minutes(&time), Time::format_seconds(&time));
     println!("Hello, world!");
 }
--- src/time.rs
+++ src/time.rs
@@ -1,46 +1,109 @@
-use std::time::{SystemTime, UNIX_EPOCH, Duration};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 const HOURS_PER_DAY: usize = 24;
 const MINUTES_PER_HOUR: usize = 60;
 const SECONDS_PER_MINUTE: usize = 60;
 
-pub struct Time {
-    unix_time: Duration,
-}
+pub struct Time;
 
+/// Used to format time in different formats
 impl Time {
+    pub fn get_current_time() -> Duration {
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("time went backwards")
+    }
 
     /**
      * Used to format the time given in seconds
      * Time presumed to be in milliseconds
      *
+     * ## Example
+     *
+     * Given 1000 milliseconds, will return 1 second
+     *
      * ## Arguments
      * * time - Time in milliseconds given as unix_time
+     *
+     * ## Returns
+     * The given time in seconds of the day
      */
     pub fn get_seconds(time: &Duration) -> usize {
         time.as_secs() as usize
     }
 
     /**
-     * Used to format the time given in minutes
+     * Used to get the time in minutes, formated in 60s
      * Time presumed to be in milliseconds
      *
      * ## Arguments
      * * time - Time in milliseconds given as unix_time
+     *
+     * ## Returns
+     * The given time in hours of the day
      */
-    pub fn get_minutes(time: &Duration) -> usize {
-        Self::get_seconds(time) * SECONDS_PER_MINUTE
+    pub fn format_seconds(time: &Duration) -> usize {
+        Self::get_seconds(time) % 60
     }
 
     /**
-     * Used to format the time given in hours
+     * Used to get the given time in minutes
+     * Time presumed to be in milliseconds
+     *
+     * ## Example
+     *
+     * Given 1000 milliseconds, will return 0 minutes
+     *
+     * ## Arguments
+     * * time - Time in milliseconds given as unix_time
+     *
+     * ## Returns
+     * The given time in minutes of the day
+     */
+    pub fn get_minutes(time: &Duration) -> usize {
+        Self::get_seconds(time) / SECONDS_PER_MINUTE
+    }
+
+    /**
+     * Used to get the time in minutes, formated in 60m
      * Time presumed to be in milliseconds
      *
      * ## Arguments
      * * time - Time in milliseconds given as unix_time
+     *
+     * ## Returns
+     * The given time in hours of the day
+     */
+    pub fn format_minutes(time: &Duration) -> usize {
+        Self::get_minutes(time) % 60
+    }
+
+    /**
+     * Used to get the given time in hours
+     * Time presumed to be in milliseconds
+     *
+     * ## Arguments
+     * * time - Time in milliseconds given as unix_time
+     *
+     * ## Returns
+     * The given time in hours of the day
      */
     pub fn get_hours(time: &Duration) -> usize {
-        Self::get_minutes(time) * MINUTES_PER_HOUR
+        Self::get_minutes(time) / MINUTES_PER_HOUR
+    }
+
+    /**
+     * Used to get the time in hours, formatted in 24h
+     * Time presumed to be in milliseconds
+     *
+     * ## Arguments
+     * * time - Time in milliseconds given as unix_time
+     *
+     * ## Returns
+     * The given time in hours of the day
+     */
+    pub fn format_hours(time: &Duration) -> usize {
+        Self::get_hours(time) % 24
     }
 }
 
@@ -50,7 +113,9 @@ mod time_test {
 
     #[test]
     fn test_seconds() {
-        let current_time = SystemTime::now().duration_since(UNIX_EPOCH).expect("time went backwards");
+        let current_time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("time went backwards");
 
         let seconds = Time::get_seconds(&current_time);
 
@@ -59,19 +124,29 @@ mod time_test {
 
     #[test]
     fn test_minutes() {
-        let current_time = SystemTime::now().duration_since(UNIX_EPOCH).expect("time went backwards");
+        let current_time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("time went backwards");
 
         let seconds = Time::get_minutes(&current_time);
 
-        assert_eq!(seconds, current_time.as_secs() as usize * SECONDS_PER_MINUTE);
+        assert_eq!(
+            seconds,
+            current_time.as_secs() as usize / SECONDS_PER_MINUTE
+        );
     }
 
     #[test]
     fn test_hours() {
-        let current_time = SystemTime::now().duration_since(UNIX_EPOCH).expect("time went backwards");
+        let current_time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("time went backwards");
 
         let seconds = Time::get_hours(&current_time);
 
-        assert_eq!(seconds, current_time.as_secs() as usize * SECONDS_PER_MINUTE * MINUTES_PER_HOUR);
+        assert_eq!(
+            seconds,
+            current_time.as_secs() as usize / SECONDS_PER_MINUTE / MINUTES_PER_HOUR
+        );
     }
 }
